c9729c1aab816dab6517e61cfc152477af19bb92
Jan Beinke <git@janbeinke.com>
2022-02-26 00:38:06 +0100

    Prefer local build for uuid evaluation

--- modules/installer-lib.nix
+++ modules/installer-lib.nix
@@ -8,10 +8,10 @@
   _module.args.wat-installer-lib = {
 
     uuidgen = { namespace ? config.wat.installer.hostUuid, name }:
-      readFile (pkgs.runCommandNoCC "uuidgenerator" {} ''
+      readFile ((pkgs.runCommandNoCC "uuidgenerator" {} ''
         uuid=$(${pkgs.util-linux}/bin/uuidgen --sha1 --namespace ${escapeShellArg namespace} --name ${escapeShellArg name})
         echo -n "$uuid" > $out
-      '');
+      '') // {preferLocalBuild = true;});
 
   };
 

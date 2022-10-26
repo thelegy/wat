4af8061c94c4c65c81843d674d8b175da31d0248
Jan Beinke <git@janbeinke.com>
2022-10-27 00:13:17 +0200

    Switch to new default package location

--- lib/baseFlake.nix
+++ lib/baseFlake.nix
@@ -8,10 +8,9 @@ let
   systems = platforms.linux;
 in rec {
 
-  packages = withPkgsFor systems nixpkgs [ self.overlay ] (pkgs: {
+  packages = withPkgsFor systems nixpkgs [ self.overlay ] (pkgs: rec {
     inherit (pkgs) wat-deploy-tools wat-deploy-tools-envrc;
+    default = wat-deploy-tools;
   });
-
-  defaultPackage = genAttrs systems (system: packages.${system}.wat-deploy-tools);
 
 }

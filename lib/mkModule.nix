78a1e857ea9261472d187be3932cf66a1e133328
Jan Beinke <git@janbeinke.com>
2023-01-10 18:33:50 +0100

    mkModule: Fix problem with disabled modules and add a bit of documentation

--- lib/mkModule.nix
+++ lib/mkModule.nix
@@ -26,18 +26,33 @@ let
       mkModule_ = { config, lib, ... }: let
         cfg = extractFromNamespace config;
         baseOptions = liftToNamespace {enable = mkEnableOption "the ${moduleName} config layer";};
-        disableModule = segments: a:
-          if length segments <= 0
-          then throw "A module may never enable itself"
-          else
-            mapAttrs (k: v:
-              if k != head segments
-              then optionalAttrs cfg.enable v
-              else disableModule (tail segments) v
-            ) a;
       in {
+
         options = recursiveUpdate baseOptions (applyIfFunction options cfg);
-        config = disableModule (moduleNamespace ++ ["enable"]) (applyIfFunction moduleConfig cfg);
+
+        #config = mkIf cfg.enable (applyIfFunction moduleConfig cfg);
+        # `mkIf` has the drawback, that it could get pushed down into not
+        # existent options, resulting in an evaluation error.
+        # Here we have the opportunity to solve this misery, as we can use some
+        # kind of `if` without pushdown and then resolve the infinite recursion
+        # by filtering out the attr path to the `enable` option.
+        # The code is more complex, but it allows modules to be much more
+        # resillient in scenarions, where they get loaded by different versions
+        # of nixpkgs or with different addionional modules loaded, which is both
+        # something this framework supports.
+        config = let
+          #disableModule : [string] -> arrtset -> attrset
+          disableModule = segments:
+            if length segments <= 0
+            then throw "A module may never enable itself"
+            else
+              mapAttrs (k: v:
+                if k != head segments
+                then mkMerge (optional cfg.enable v)
+                else disableModule (tail segments) v
+              );
+        in disableModule (moduleNamespace ++ ["enable"]) (applyIfFunction moduleConfig cfg);
+
       };
     in { imports = [ mkModule_ ]; };
 

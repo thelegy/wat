0ee89dff0a42b74db7fe0079a20af618fc9d0849
Jan Beinke <git@janbeinke.com>
2026-04-06 12:46:29 +0200

    Add existing deploy commands to wat-tool without functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(name = "wat")]
@@ -9,7 +9,30 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Todo,
+    /// Run "Boot" + "Test"
+    Switch(CommonBuildArgs),
+
+    Boot(CommonBuildArgs),
+
+    /// Run "Boot" and then perfrom reboot to activate
+    Reboot(CommonBuildArgs),
+
+    Test(CommonBuildArgs),
+
+    DryActivate(CommonBuildArgs),
+
+    /// Just build machine and print store path
+    Build(CommonBuildArgs),
+
+    Iso(CommonBuildArgs),
+
+    Sdcard(CommonBuildArgs),
+}
+
+#[derive(Debug, Args)]
+struct CommonBuildArgs {
+    /// The name of the host to operate on
+    hostname: String,
 }
 
 fn main() {

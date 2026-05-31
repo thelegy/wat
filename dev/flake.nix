95dd461f388371ac273cecc17aee0623a9007e45
Jan Beinke <git@janbeinke.com>
2026-05-31 11:14:13 +0200

    dev/flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'crane':
        'github:ipetkov/crane/6c23998526351a53ce734f0ac84940da988ccef1' (2026-04-03)
      → 'github:ipetkov/crane/0532eb17955225173906d671fb36306bdeb1e2dc' (2026-05-30)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/57928607ea566b5db3ad13af0e57e921e6b12381' (2026-02-02)
      → 'github:hercules-ci/flake-parts/f7c1a2d347e4c52d5fb8d10cb4d94b5884e546fb' (2026-05-13)
    • Updated input 'git-hooks-nix':
        'github:cachix/git-hooks.nix/5eaaedde414f6eb1aea8b8525c466dc37bba95ae' (2026-02-10)
      → 'github:cachix/git-hooks.nix/61ab0e80d9c7ab14c256b5b453d8b3fb0189ba0a' (2026-05-11)
    • Updated input 'import-tree':
        'github:vic/import-tree/c968d3b54d12cf5d9c13f16f7c545a06c9d1fde6' (2026-02-14)
      → 'github:vic/import-tree/d321337efd0f23a9eb14a42adb7b2c29313ab274' (2026-05-14)
    • Updated input 'nixpkgs':
        'https://releases.nixos.org/nixpkgs/nixpkgs-26.05pre944764.2343bbb58f99/nixexprs.tar.xz?lastModified=1770843696&narHash=sha256-9SFCZkVcpDOV6unH5hVEy4%2BdB0rxMuUoBnDAO6vshac%3D&rev=2343bbb58f99267223bc2aac4fc9ea301a155a16' (2026-02-11)
      → 'https://releases.nixos.org/nixpkgs/nixpkgs-26.11pre1006943.e9a7635a5759/nixexprs.tar.xz?lastModified=1780030872&narHash=sha256-j1JL8PtaAVdH84cV%2BScicmrck1tX0CM60F31md5C/MM%3D&rev=e9a7635a57597d9754eccebdfc7045e6c8600e6b' (2026-05-29)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/337a4fe074be1042a35086f15481d763b8ddc0e7' (2026-02-04)
      → 'github:numtide/treefmt-nix/790751ff7fd3801feeaf96d7dc416a8d581265ba' (2026-04-08)

--- dev/flake.nix
+++ dev/flake.nix
@@ -1,33 +1,29 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     crane.url = "github:ipetkov/crane";
     flake-compat = {
-      flake = false;
       url = "github:NixOS/flake-compat";
+      flake = false;
     };
-    flake-file.url = "github:vic/flake-file";
+    flake-file.url = "github:denful/flake-file";
     flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     git-hooks-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-    nixpkgs-lib.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
     treefmt-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     wat.url = "github:input-output-hk/empty-flake";
   };
-
 }
--- dev/modules/dendritic.nix
+++ dev/modules/dendritic.nix
@@ -4,7 +4,7 @@
     (inputs.flake-file.flakeModules.dendritic or { })
   ];
 
-  flake-file.inputs.flake-file.url = "github:vic/flake-file";
+  flake-file.inputs.flake-file.url = "github:denful/flake-file";
 
   flake-file.inputs.flake-compat.flake = false;
   flake-file.inputs.flake-compat.url = "github:NixOS/flake-compat";
